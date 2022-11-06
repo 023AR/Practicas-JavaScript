2901b2b53c82f2c669073405a989263a90f54780
Rodrigo Colque Garcia <colqueariel023@gmail.com>
2022-11-06 14:06:25 -0400

    Se acabo la funcion Fibonacci

--- Practica_4/script.js
+++ Practica_4/script.js
@@ -1,7 +1,26 @@
-const numeroCalcular = document.querySelector('#numeroFibonacci')
+const numCalcular = parseInt(document.querySelector('#numeroFibonacci'));
+const btnFib = document.querySelector('#btnCalcular')
 
-function calcularFibonacci(){
-  return 0;
+btnFib.addEventListener('click', () =>{
+  document.querySelector('#resultado').innerText = valor;
+
+})
+
+function calcFibo(numero){
+  fibonaccioText = '0';
+
+  fibonaccioAntes = 0;
+  auxi = 0;
+  fibonacciUltimo = 1;
+  for(let i = 0; i < numero; i++){
+    fibonaccioText += ' ' + fibonacciUltimo; // 0 1 1 2 3 5
+    // 3
+    auxi = fibonaccioAntes;  // 2
+    // 5
+    fibonaccioAntes = fibonacciUltimo;  // 1
+    // 5               3         5
+    fibonacciUltimo = auxi + fibonacciUltimo;  // 1
+  }
 }
 
-document.querySelector('#resultado').innerText = calcularFibonacci();
+// Clase 16
