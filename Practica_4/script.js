bdd74537a48da1bc0f03a4b42f12b1ac0deaa942
Rodrigo Colque Garcia <colqueariel023@gmail.com>
2022-11-06 09:32:26 -0400

    Empezamos con la funcion de calcularFibonacci en JavaScript

---
+++ Practica_4/script.js
@@ -0,0 +1,7 @@
+const numeroCalcular = document.querySelector('#numeroFibonacci')
+
+function calcularFibonacci(){
+  return 0;
+}
+
+document.querySelector('#resultado').innerText = calcularFibonacci();
