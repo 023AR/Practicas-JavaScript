96633008622f7e36263854c9e57510844d10b1a1
Rodrigo Colque Garcia <colqueariel023@gmail.com>
2022-11-08 07:49:53 -0400

    Se concluyo la funcion Fibonnaci

--- Practica_4/script.js
+++ Practica_4/script.js
@@ -13,13 +13,10 @@ function calcFibo(numero){
   auxi = 0;
   fibonacciUltimo = 1;
   for(let i = 0; i < numero; i++){
-    fibonaccioText += ' ' + fibonacciUltimo; // 0 1 1 2 3 5
-    // 3
-    auxi = fibonaccioAntes;  // 2
-    // 5
-    fibonaccioAntes = fibonacciUltimo;  // 1
-    // 5               3         5
-    fibonacciUltimo = auxi + fibonacciUltimo;  // 1
+    fibonaccioText += ' ' + fibonacciUltimo; 
+    auxi = fibonaccioAntes; 
+    fibonaccioAntes = fibonacciUltimo;  
+    fibonacciUltimo = auxi + fibonacciUltimo; 
   }
 }
 
